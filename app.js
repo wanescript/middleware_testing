e0f2a385b30332eaf552ccac2c457bfe50e6d3f3
handmakerzhm <handmakerzhm@gmail.com>
2020-07-17 22:38:47 -0700

    testing commits

--- app.js
+++ app.js
@@ -15,19 +15,8 @@ app.get('/', (req,res)=> {
     res.send('homepage. Welcome to da spot visitor...!');
     let timestamp = Date.now();
     console.log(`User ip is :${req.ip} at the time of${timestamp} with a ${os.platform()} OS`)
-    console.log(os.type());
-    console.log(os.uptime());
-    console.log(os.userInfo([]));
-    console.log(os.hostname());
-    console.log(os.freemem());
-    console.log(os.cpus());
-    
+  
 });
-
-app.get('/homepage',(req,res)=>{
-    res.send('side page')
-    console.log('logging morgan')
-})
 
 
 app.listen(port,()=>console.log(`Server is running on port ${port}`));
